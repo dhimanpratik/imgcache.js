fcf4b60f45005b5dc3dcea03afab52d635b63f25
Pratik Dhiman <pd46199n@pace.edu>
2015-12-02 12:59:36 -0500

    Update package.js

--- chrisben:imgcache/package.js
+++ chrisben:imgcache/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "JS library that stores images locally for offline apps using PhoneGap/Cordova or browsers supporting the new html5 File API",
   version: "1.0rc2",
-  git: "https://github.com/chrisben/imgcache.js"
+  git: "https://github.com/dhimanpratik/imgcache.js"
 });
 
 Package.onUse(function(api) {
